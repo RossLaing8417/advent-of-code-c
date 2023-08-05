d7acc79f2628c5a03033ba900a989394f5f71ad8
Ross Laing <30736276+RossLaing8417@users.noreply.github.com>
2023-08-05 15:11:12 +0200

    2015 day 4

---
+++ 2015/04/src/part2.c
@@ -0,0 +1,57 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "md5.h"
+
+#define MAX_SIZE 16
+
+void print_hash(uint8_t *p){
+    for(unsigned int i = 0; i < 16; ++i){
+        printf("%02x", p[i]);
+    }
+    printf("\n");
+}
+
+int main(int argc, char * argv[]) {
+    if (argc < 2) {
+        printf("File as first argument required\n");
+        exit(-1);
+    }
+
+    const char * filename = argv[1];
+
+    FILE * file = fopen(filename, "r");
+
+    if (!file) {
+        printf("Error reading file: %s\n", filename);
+        exit(-1);
+    }
+
+    char key[MAX_SIZE];
+    char message[32];
+
+    fgets(key, sizeof(key), file);
+    key[strcspn(key, "\n")] = 0;
+
+    uint8_t result[16];
+    size_t i = 0;
+
+    while (1) {
+        i++;
+
+        sprintf(message, "%s%zu", key, i);
+
+        md5String(message, result);
+
+        // There is probably some bit stuff I could do...
+        if (!result[0] && !result[1] && !result[2]) {
+            // printf("Magic!\n");
+            break;
+        }
+    }
+
+    printf("Answer: %zu\n", i);
+
+    fclose(file);
+}
